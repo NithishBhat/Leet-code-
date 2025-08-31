e668218c75687fd97da2f15b152237d7f7c2d5cf
Nithish Bhat <nithishbhat1998@gmail.com>
2025-08-31 08:41:57 -0400

    Update LongestContinuousPrefix.py

--- LongestContinuousPrefix/LongestContinuousPrefix.py
+++ LongestContinuousPrefix/LongestContinuousPrefix.py
@@ -5,15 +5,6 @@ Created on Sat Aug 30 22:20:01 2025
 @author: Red
 """
 
-strs=["flower","flow","flight"]
 
-common=strs[0]
-temp=""
-for i in strs[1:]:
-    for j,k in zip(i,common) :
-        if j==k:
-            temp=temp+j
         else:
             break
-    common=temp
-    temp=""
