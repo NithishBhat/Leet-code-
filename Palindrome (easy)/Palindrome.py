06f9001736441dcbdc66198bb1ffaff6c537706b
Nithish Bhat <nithishbhat1998@gmail.com>
2025-08-30 22:08:17 -0400

    plaindrom

---
+++ Palindrome (easy)/Palindrome.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 30 15:40:16 2025
+
+@author: Red
+
+httpsleetcode.comproblemspalindrome-numbersubmissions1754004561
+"""
+
+x=str(122)
+y=x[::-1]
+
+print(x==y)
+    
